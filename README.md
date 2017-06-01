6affc7ce500c5ee34a10bc50c0954bda0c2162ef
gomesuit <gomesuit@gmail.com>
2017-06-02 03:02:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# sqlite
+
+```bash
+# データベースの作成
+sqlite3 sample.sqlite
+
+# テーブル一覧
+.table
+
+# スキーマを表示
+.schema <table_name>
+
+# 外部SQLファイルを実行
+.read test.sql
+
+# ヘルプ
+.help
+
+# バックアップ
+.output backup.dump
+.dump [table]
+
+# 設定情報を表示する
+.show
+```
---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# install common package
+sudo yum install -y vim
+
+# install dependency package
+sudo yum install -y gcc
+
+# install sqlite
+wget http://www.sqlite.org/2017/sqlite-autoconf-3190200.tar.gz
+tar zxfv sqlite-autoconf-3190200.tar.gz
+cd sqlite-autoconf-3190200
+./configure --prefix=/usr/local
+make
+make install
+sudo make install
+sqlite3 -version
